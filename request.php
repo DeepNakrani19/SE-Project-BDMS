0ff32e621feee343ad5b7755cbdfa9a4377505df
Deep Nakrani <56976010+DeepNakrani19@users.noreply.github.com>
2021-10-17 20:34:25 +0530

    Add files via upload

---
+++ request.php
@@ -0,0 +1,38 @@
+<?php 
+define("DB_SERVER", "localhost:3306");
+define("DB_USER", "root");
+define("DB_PASSWORD", "");
+define("DB_DATABASE", "bdms");
+
+$cnn =	new mysqli(DB_SERVER , DB_USER, DB_PASSWORD, DB_DATABASE);
+if (mysqli_connect_errno()) 
+{
+printf("Connect failed: %s\n", mysqli_connect_error());
+exit();
+}
+	$name = $_POST ['name'];
+	$phone = $_POST ['num'];
+	$email = $_POST ['mail'];
+	$birth = $_POST['dob'];
+	$bgroup = $_POST ['bgroup'];
+    $location = $_POST ['location'];
+    $address = $_POST ['addr'];
+	
+	$sql = ("INSERT INTO request (pname,phone_no,email,dob,rblood_group,plocation,paddress) VALUES ('$name','$phone','$email','$birth','$bgroup','$location','$address')");
+		if ($cnn->query($sql) === TRUE) 
+            {
+               echo "Request made successfully!!!";
+               echo " We will contact you soon regarding appointment:)";
+  			//sleep(10);
+			 
+  			 header('Refresh: 10; URL=http://localhost/bdms/home.html');
+  			
+           //echo"<meta http-equiv="refresh" content="5;url=login.html">"
+            } 
+            else {
+               echo "Error: " . "" . mysqli_error($cnn);
+            }
+
+	
+
+ ?>
